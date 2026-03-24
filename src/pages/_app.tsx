2ff2900c486953b5bd1b2f8ad265143e1febdc5e
Ravi Gupta <raviranjankumargupta@Ravis-MacBook-Air.local>
2026-03-24 14:45:20 +0530

    Add external advertisement script to _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,15 @@
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
+import Script from 'next/script';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Script 
+        src="https://pl28969267.profitablecpmratenetwork.com/a4/12/87/a41287224eec82ac91df41f92d0f54f1.js" 
+        strategy="afterInteractive" 
+      />
+      <Component {...pageProps} />
+    </>
+  );
 }
